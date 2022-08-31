0f185a51cdcb8ecd6890a0b5582b6306b1c7b018
Mutoi01 <pngando9@gmail.com>
2022-08-31 12:42:24 +0300

    commit tree 04ee88968b24313f8c8045d61228cd8ca1fd9134

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
   # write code here
+  attr_accessor :height, :hypotenuse, :lenght
+
+  def initialize (lenght,height,hypotenuse)
+    @lenght = lenght
+    @height = height
+    @hypotenuse = hypotenuse
+  end
+
+  def kind
+    if lenght==height && height==hypotenuse
+      :equilateral
+
+    elsif lenght==height || height==hypotenuse ||lenght==hypotenuse
+     :isosceles
+
+    else
+      :scalene
+    end
+  end
+
+
+  class TriangleError < StandardError
+    
+  end
+
 end
